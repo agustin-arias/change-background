a8062218460f0ed47da9b5a666b5ad74791109e1
agustinntarias <gustin444@gmail.com>
2021-01-20 13:45:06 -0300

    Added error redirection

--- change-background.sh
+++ change-background.sh
@@ -59,16 +59,16 @@ downloadImage(){
     # random number:
     randomImage=$((2+$RANDOM%15))
     xpathImage="${xpathGeneral}"'/div[@class="content col-md-12 col-xs-12"]/ul[@class="element"]/li['"${randomImage}"']/a/@href'
-    image=$(curl -s $images | xmllint --html --xpath "string($xpathImage)" -)
+    image=$(curl -s $images | xmllint --html --xpath "string($xpathImage)" - 2>/dev/null)
     imagePage="${mainPage}${image}"
 
     xpathDownload="${xpathGeneral}"'/div[@class="content col-md-12 col-xs-12"]/div[@class="lig-cnt col-md-8"]/div[@class="download-wallpaper"]/ul[1]/li[2]/a/@href'
-    downloadWallpaper=$(curl -s $imagePage | xmllint --html --xpath "string($xpathDownload)" -)
+    downloadWallpaper=$(curl -s $imagePage | xmllint --html --xpath "string($xpathDownload)" - 2>/dev/null)
     downloadWallpaperPage="${mainPage}${downloadWallpaper}"
 
     # complete
     xpathFullImage="${xpathGeneral}"'/div[@class="this_download col-md-12 col-xs-12"]/div[@class="full-img col-md-9"]/img/@src'
-    fullImage=$(curl -s $downloadWallpaperPage | xmllint --html --xpath "string($xpathFullImage)" -)
+    fullImage=$(curl -s $downloadWallpaperPage | xmllint --html --xpath "string($xpathFullImage)" - 2>/dev/null)
 
     nameFullImage=$(echo $fullImage | grep -Eow "[^\/]*\.jpg$" -)
     pathToImage='/home/matheth/Pictures/desktop-wallpapers/'"${nameFullImage}"
